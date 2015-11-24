564118bc60f8ac24a8a9362edf393ebb81b4ea85
unknown <purushottam.kumar@user492.aepl.com>
2015-11-24 16:08:49 +0530

    Addition: Initial google analytics code

--- app/src/main/java/com/krp/android/demogoogleanalytics/MainActivity.java
+++ app/src/main/java/com/krp/android/demogoogleanalytics/MainActivity.java
@@ -1,37 +1,125 @@
 package com.krp.android.demogoogleanalytics;
 
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static String TAG = MainActivity.class.getSimpleName();
+
+    private Toolbar mToolbar;
+
+    private Button btnSecondScreen, btnSendEvent, btnException, btnAppCrash, btnLoadFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        btnSecondScreen = (Button) findViewById(R.id.btnSecondScreen);
+        btnSendEvent = (Button) findViewById(R.id.btnSendEvent);
+        btnException = (Button) findViewById(R.id.btnException);
+        btnAppCrash = (Button) findViewById(R.id.btnAppCrash);
+        btnLoadFragment = (Button) findViewById(R.id.btnLoadFragment);
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+        /**
+         * Launching another activity to track the other screen
+         */
+        btnSecondScreen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(intent);
+            }
+        });
 
-        return super.onOptionsItemSelected(item);
+        /**
+         * Tracking Fragment View
+         */
+        btnLoadFragment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                FooterFragment footerFragment = new FooterFragment();
+                fragmentTransaction.replace(R.id.frame_container, footerFragment);
+                fragmentTransaction.commit();
+            }
+        });
+
+        /**
+         * Event tracking
+         * Event(Category, Action, Label)
+         */
+        btnSendEvent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Tracking Event
+                MyApplication.getInstance().trackEvent("Book", "Download", "Track event example");
+
+                Toast.makeText(getApplicationContext(), "Event \'Book\' \'Download\' \'Event example\' is sent. Check it on Google Analytics Dashboard!", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        /**
+         * Tracking Exception Manually
+         * All known exceptions can be tracking this way
+         * using Try & Catch
+         */
+        btnException.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    String name = null;
+                    if (name.equals("ravi")) {
+                    /* Never comes here as it throws null pointer exception */
+                    }
+                } catch (Exception e) {
+                    // Tracking exception
+                    MyApplication.getInstance().trackException(e);
+
+                    Toast.makeText(getApplicationContext(), getString(R.string.toast_track_exception), Toast.LENGTH_LONG).show();
+
+                    Log.e(TAG, "Exception: " + e.getMessage());
+                }
+            }
+        });
+
+        /**
+         * Tracking App Crashes
+         * Manually generation app crash by dividing with zero
+         */
+        btnAppCrash.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Toast.makeText(getApplicationContext(), getString(R.string.toast_app_crash), Toast.LENGTH_LONG).show();
+
+                Runnable r = new Runnable() {
+                    @Override
+                    public void run() {
+                        int answer = 12 / 0;
+                    }
+                };
+
+                Handler h = new Handler();
+                h.postDelayed(r, 1500);
+            }
+        });
     }
 }
